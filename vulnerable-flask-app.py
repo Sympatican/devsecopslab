663ff0461cc520749fbefdcfb3169412c0ca4efe
Sympatican <148090419+Sympatican@users.noreply.github.com>
2023-10-18 14:48:24 +0200

    Update vulnerable-flask-app.py

--- vulnerable-flask-app.py
+++ vulnerable-flask-app.py
@@ -136,7 +136,7 @@ def create_file():
 
 connection = {}
 max_con = 50
-key = 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
+key = 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
 
 def factorial(number):
     if number == 1:
